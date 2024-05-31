8a453a276c14124dae6d50cb8acdfaf026357fe5
anujagit11 <saykhede29@gmail.com>
2024-05-31 11:47:27 +0530

    udated

--- UserCrudOperation-TeamtoCrud/src/main/java/com/userCrudOperation/controller/HomeController.java
+++ UserCrudOperation-TeamtoCrud/src/main/java/com/userCrudOperation/controller/HomeController.java
@@ -1,5 +1,10 @@
 package com.userCrudOperation.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+	
+	
 
 }
--- UserCrudOperation-TeamtoCrud/src/main/java/com/userCrudOperation/repo/UserRepo.java
+++ UserCrudOperation-TeamtoCrud/src/main/java/com/userCrudOperation/repo/UserRepo.java
@@ -1,9 +1,11 @@
 package com.userCrudOperation.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.userCrudOperation.model.User;
 
+@Repository
 public interface UserRepo extends JpaRepository<User, Integer>{
 
 }
